e54568429b317b5d4fbbac4fcdfe3bc791c40f86
ZachMommaerts <zmommaerts@gmail.com>
2023-10-20 19:03:45 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Zach Mommaerts on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
