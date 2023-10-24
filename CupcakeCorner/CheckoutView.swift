896ea0a7048847f5e3863297074af19e05540081
ZachMommaerts <zmommaerts@gmail.com>
2023-10-23 23:45:04 -0500

    - Create AddressView - Update Order class for AddressView

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Zach Mommaerts on 10/23/23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
